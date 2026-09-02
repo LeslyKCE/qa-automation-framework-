4e9a89cb031273b45ecff4ad8d8ff14e8869edfd
Lesly Chavez <lchavez.ucs@gmail.com>
2026-09-02 18:06:23 -0500

    dia 2 operadores

---
+++ ejercicios/dia02.js
@@ -0,0 +1,42 @@
+//Operadores
+const a = 10;
+const b = 5;
+
+console.log (a + b)
+
+//Residuo
+const numero = 10;
+console.log (numero % 2)
+
+const n = 14;
+console.log (n % 2)
+
+const n2 = 14;
+console.log (n2 % 4)
+
+//Multiplicación
+const precioProducto = 100;
+const cantidad = 3;
+const subtotal = precioProducto * cantidad;
+console.log (subtotal)
+
+//Resta
+const descuento = 50;
+const total = subtotal - descuento;
+console.log (total)
+
+//División
+const totalPago = 250;
+const nroCuotas = 5;
+const valorCuota = totalPago / nroCuotas;
+console.log (valorCuota)
+
+//Operadores de comparación
+//1. ==
+const estadoEsperado = "Activo";
+const estadoObtenido = "Activo";
+console.log (estadoEsperado == estadoObtenido)
+
+const estadoEsperado1 = "Activo";
+const estadoESperado2 = "Inactivo";
+console.log (estadoEsperado1 == estadoESperado2)
